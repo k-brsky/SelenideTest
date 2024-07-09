2eb73d0e839950e97751ec9db8d6f09b70f0148c
Kristina Borisovskaya <krissstinkaaa163@gmail.com>
2024-07-09 17:00:49 +0400

    add test for GitHub and Selenide

---
+++ src/test/java/SelenideGithubTest.java
@@ -0,0 +1,35 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
+
+public class SelenideGithubTest {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://github.com";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.timeout = 5000;
+    }
+
+    @Test
+    void findSoftAssertions(){
+        open("/selenide/selenide");
+        $("#wiki-tab").click();
+        $("#wiki-pages-filter").setValue("SoftAssertions");
+        $("#wiki-pages-box").$(byText("SoftAssertions")).click();
+        $("#wiki-body").shouldHave(text("JUnit5"));
+        $("#wiki-body").shouldHave(text("@ExtendWith({SoftAssertsExtension.class})\n" +
+                "class Tests {\n" +
+                "@Test\n" +
+                "void test() {\n" +
+                "Configuration.assertionMode = SOFT;\n" +
+                "open(\"page.html\");\n" +
+                "$(\"#first\").should(visible).click();\n" +
+                "$(\"#second\").should(visible).click();\n"));
+    }
+}
